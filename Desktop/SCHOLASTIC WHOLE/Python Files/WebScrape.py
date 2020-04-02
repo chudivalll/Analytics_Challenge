d43edaf5160f5aa0c836adfd78bd6c03f2573dd4
greendreamdj <26914863+greendreamdj@users.noreply.github.com>
2020-04-01 22:56:38 -0400

    first commit

---
+++ Desktop/SCHOLASTIC WHOLE/Python Files/WebScrape.py
@@ -0,0 +1,42 @@
+#packages
+from selenium import webdriver
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+import pyodbc
+import pandas
+
+#Dataset
+server = 'localhost'
+database = 'Scholastic'
+username = 'AIS_challenge'
+password = 'ScholasticChallenge'
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+password)
+cursor = cnxn.cursor()
+
+df = pandas.read_sql_query(
+'''
+SELECT*FROM ZipcodeStats
+''', cnxn)
+
+df2 = pandas.Series([])
+i = 0 
+for i in range(len(df)):
+    #The website
+    my_url = 'http://www.mapszipcode.com/florida/miami/'+ str(df.iloc[i,0]) + '/'
+    
+    #opens up a connection, grabbing the page
+    uClient = uReq(my_url)
+    page_html = uClient.read()
+    uClient.close()
+    
+    # html parsing
+    page_soup = soup(page_html, "html.parser")
+    
+    #Output
+    containers = page_soup.findAll("div",{"class":"dat"})[0].text
+    df2[i] = containers
+    i = i + 1
+
+df.insert(6,"Population", df2)
+#%%
+df.to_csv('ZipcodeStatistics.csv',index=False)
